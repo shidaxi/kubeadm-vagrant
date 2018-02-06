c323ec5d01803a7a9505d3ced2f228b544ca7af5
gao <gao@GAOs-MacBook-Pro.local>
2018-02-06 15:19:11 +0800

    1

--- CentOS/install-centos.sh
+++ CentOS/install-centos.sh
@@ -11,7 +11,7 @@ gpgkey=https://packages.cloud.google.com/yum/doc/yum-key.gpg
         https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
 EOF
 setenforce 0
-yum install -y net-tools screen tree telnet kubelet kubeadm docker --nogpgcheck
+yum install -y net-tools bind-utils screen tree telnet kubelet kubeadm docker --nogpgcheck
 systemctl enable kubelet && systemctl start kubelet
 systemctl enable docker && systemctl start docker
 
@@ -19,5 +19,6 @@ setenforce 0
 sed -i 's/SELINUX=enforcing/SELINUX=enforcing/g' /etc/selinux/config
 swapoff -a
 sed -i '/swap/s/^/#/g' /etc/fstab
+sed -i '/search/d' /etc/resolv.conf
 
 echo '1' > /proc/sys/net/bridge/bridge-nf-call-iptables
